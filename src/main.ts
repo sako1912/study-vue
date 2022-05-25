4c4c24fe1bb6cacb7ee6752c87c5a502b850a29a
sako <ksa1912@naver.com>
2022-05-25 22:09:55 +0900

    vuetify 적용

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI, { size: "small", zIndex: 3000 });
@@ -11,5 +12,6 @@ Vue.use(ElementUI, { size: "small", zIndex: 3000 });
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
